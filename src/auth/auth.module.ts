bd520b5c3d20a2005165b8ef766795cbfa20320f
Harys Vizcaino <harysvizcaino@gmail.com>
2025-08-20 14:26:39 -0400

    Refactor authentication module and enhance SeedService tests
    - Remove unused UsersService import from auth.module.ts for cleaner code.
    - Update SeedService tests to include a mock implementation of UsersService, improving test isolation and reliability.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,7 +3,6 @@ import { ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
-import { UsersService } from 'src/users/users.service';
 import { HashHelper } from 'src/helpers/hashing-helpers';
 import { AuthGuard } from './guards/jwt-auth.guard';
 import { APP_GUARD } from '@nestjs/core';
--- src/seed/seed.service.spec.ts
+++ src/seed/seed.service.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SeedService } from './seed.service';
+import { UsersService } from 'src/users/users.service';
 
 describe('SeedService', () => {
   let service: SeedService;
 
+  const mockUsersService = {
+    create: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SeedService],
+      providers: [
+        SeedService,
+        {
+          provide: UsersService,
+          useValue: mockUsersService,
+        },
+      ],
     }).compile();
 
     service = module.get<SeedService>(SeedService);
